ce9eb57318030cff3bf3d16ce08c0870b7097dea
Intavega1~ <intavega@hotmail.com>
2022-03-06 18:22:39 -0500

    VARIABLES Y TIPOS EN php

---
+++ api.php
@@ -0,0 +1,17 @@
+<?php
+$nomUsu = (string) "Ingrid Tatiana Vesga Gavanzo";
+$edadUsu =(int) 37; 
+$altUsu= (float) 1.55;
+$HobUsu = (array) [
+    (string) "Manualidades",
+    (string) "cine",
+    (string) "musica",
+];
+$soyInt = (bool) true;
+$rutUsu = (object) [];
+$rutUsu -> levantar = (int) 6;
+$vacio = null;
+$notengoDependencia = uninitialized;
+
+echo "";
+?>
